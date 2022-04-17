fa0735c734827180f4fc7a10734f0a43c86272e8
berkebyrktr <1910205084@ogrenci.karabuk.edu.tr>
2022-04-17 23:26:23 +0300

    Template

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']) -> name('home');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
